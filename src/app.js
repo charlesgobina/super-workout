4fc2d6d3cc957154f16e770b36dee4b7eb75146b
charlesgobina <charles.gobina@gmail.com>
2022-06-25 22:25:38 +0100

    Finish set up

--- src/app.js
+++ src/app.js
@@ -1,5 +1,21 @@
+import React from 'react';
+import { Route, Routes } from 'react-router-dom';
+import './app.css';
+import { Box } from '@mui/system';
+import Navbar from './components/Navbar';
+import Footer from './components/Footer';
+import Home from './pages/Home';
+import ExerciseDetail from './pages/ExcerciseDetail';
+
 const App = () => (
-  <p>Hello World</p>
+  <Box width="400px">
+    <Navbar />
+    <Routes>
+      <Route path="/" element={<Home />} />
+      <Route path="/exercise/:id" element={<ExerciseDetail />} />
+    </Routes>
+    <Footer />
+  </Box>
 );
 
 export default App;
--- src/index.js
+++ src/index.js
@@ -1,8 +1,8 @@
 import React from 'react';
-import { ReactDOM } from 'react-dom';
+import { createRoot } from 'react-dom/client';
 import App from './app';
 
-const root = ReactDOM.creatRoot(document.getElementById('root'));
+const root = createRoot(document.getElementById('root'));
 
 root.render(
   <App />,
